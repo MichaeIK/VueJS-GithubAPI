625759eb79feb189e007425f2299d875a1de70c6
Michael Korzun <31041089+MichaeIK@users.noreply.github.com>
2018-04-30 15:14:23 +0300

    add user check before mutate state

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 export const mutations = {
 	addGithubUser (state, userData ) {
-		Vue.set(state.listOfGithubUsers, userData.id, userData)
+		userData.id && Vue.set(state.listOfGithubUsers, userData.id, userData)
 	},
 	deleteGithubUser (state, id) {
 		Vue.delete(state.listOfGithubUsers, id)
