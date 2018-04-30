0930530e61a8465e02cea111cfc3525b20b6a51f
Michael Korzun <31041089+MichaeIK@users.noreply.github.com>
2018-04-30 14:48:51 +0300

    pass data as props to component

--- src/components/Michael.vue
+++ src/components/Michael.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<div class="hello">
+		<div>
 			<h1>{{ msg }}</h1>
 			<p v-if="loading">Loading ...</p>
 			<p class="error" v-if="error">{{error}}</p>
@@ -9,24 +9,22 @@
 			<button v-on:click="addUser">Look for github user</button>
 		</div>
 		<ul class="githubUserListWrap">
-			<li class="githubUser" v-for="item in this.$store.state.listOfGithubUsers" :key="item.id">
-				<span class="githubUserDeleteButton" v-on:click="deleteGithubUserFromList(item.id)">X</span>
-				<span>{{item.login}}</span>
-				<img class="githubUserAva" :src="item.avatar_url" />
-				<ul class="githubUseerStats">
-					<li><span>Repos:</span> {{ item.public_repos }}</li>
-					<li><span>Followers:</span> {{ item.followers }}</li>
-					<li><span>Following:</span> {{ item.following }}</li>
-				</ul>
-			</li>
+			<UserCard
+				v-for="item in this.$store.state.listOfGithubUsers"
+				:key="item.id"
+				:item="item"
+				:deleteGithubUserFromList="deleteGithubUserFromList">
+			</UserCard>
 		</ul>
 	</div>
 </template>
 
 <script>
+import UserCard from './UserCard';
 import { mapActions, mapGetters } from 'vuex'
 export default {
 	name: 'Micheal',
+	components: { UserCard },
 	data () {
 		return {
 			loading: false,
@@ -57,35 +55,8 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-.error {
-	color: red;
-}
 .githubUserListWrap {
 	list-style: none;
 	display: flex;
-}
-.githubUser {
-	width: 130px;
-	border: 1px solid black;
-	margin: 0 15px;
-	position: relative;
-}
-.githubUserDeleteButton {
-	position: absolute;
-	right: 0;
-	color: red;
-	cursor: pointer;
-
-}
-.githubUserAva {
-	width: 100px;
-	border-radius: 50%;
-}
-.githubUseerStats {
-	padding: 0;
-	list-style: none;
-}
-.githubUseerStats span {
-	font-weight: bold;
 }
 </style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="githubUserCard">
+        <span class="githubUserDeleteButton" v-on:click="deleteGithubUserFromList(item.id)">X</span>
+        <span>{{item.login}}</span>
+        <img class="githubUserAva" :src="item.avatar_url" />
+        <ul class="githubUseerStats">
+            <li><span>Repos:</span> {{ item.public_repos }}</li>
+            <li><span>Followers:</span> {{ item.followers }}</li>
+            <li><span>Following:</span> {{ item.following }}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: 'GithubUserCard',
+  props: {
+    item: Object,
+    deleteGithubUserFromList: Function
+  }
+}
+</script>
+<style scoped>
+.githubUserCard {
+	width: 130px;
+	border: 1px solid black;
+	margin: 0 15px;
+	position: relative;
+}
+.githubUserDeleteButton {
+	position: absolute;
+	right: 5px;
+	color: red;
+	cursor: pointer;
+
+}
+.githubUserAva {
+	width: 100px;
+	border-radius: 50%;
+}
+.githubUseerStats {
+	padding: 0;
+	list-style: none;
+}
+.githubUseerStats span {
+	font-weight: bold;
+}
+</style>
+
+
